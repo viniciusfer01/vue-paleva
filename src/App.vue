a16a513adc33fbc229851d4cde344321897c7222
Vinícius Fernandes <business.fernandes22@gmail.com>
2024-11-14 19:57:18 -0300

    user sees dummy orders

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,27 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import ListOrders from './components/ListOrders.vue'
+import { ref } from 'vue'
+
+const storeCode = ref('')
+const showOrdersComponent = ref(false)
+
+const showOrders = () => {
+  if (storeCode.value) {
+    showOrdersComponent.value = true
+  } else {
+    showOrdersComponent.value = false
+  }
+}
 </script>
 
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h1>PaLevá - Kitchen Edition</h1>
+    <div v-if="!showOrdersComponent">
+      <input type="text" placeholder="Search for orders from store..." v-model="storeCode"/>
+      <button @click="showOrders">Show Orders</button>
+    </div>
+    <ListOrders v-if="showOrdersComponent" :storeCode="storeCode"/>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
---
+++ src/components/ListOrders.vue
@@ -0,0 +1,67 @@
+<script setup>
+import { ref } from 'vue'
+
+const DUMMY_ORDERS = [
+  {
+    id: 1,
+    name: 'Zezin',
+    phone: '123-456-7890',
+    email: 'dummy@email.com',
+    cpf: '123.456.789-00',
+    status: 'pending',
+    code: 'ABC123VS',
+    price: 50.00,
+    userId: 1,
+    storeId: 1,
+    createdAt: '2021-09-01T12:00:00',
+    updatedAt: '2021-09-01T12:00:00',
+  },
+  {
+    id: 2,
+    name: 'Jorgin',
+    phone: '123-456-7890',
+    email: 'Jorgin@email.com',
+    cpf: '123.456.789-00',
+    status: 'pending',
+    code: 'ABC456VS',
+    price: 30.00,
+    userId: 2,
+    storeId: 1,
+    createdAt: '2021-09-01T12:00:00',
+    updatedAt: '2021-09-01T12:00:00',
+  },
+]
+
+const props = defineProps({
+  storeCode: String
+})
+
+</script>
+
+<template>
+  <h2>Pedidos para a loja: {{ storeCode }}</h2>
+  <a href="/">Voltar</a>
+  <ul>
+    <li v-for="order in DUMMY_ORDERS" :key="order.id">
+      <!-- <p>{{ order.name }}</p>
+      <p>{{ order.phone }}</p>
+      <p>{{ order.email }}</p>
+      <p>{{ order.cpf }}</p> -->
+      <a href="{{ order.code }}">{{ order.code }}</a>
+      <p>{{ order.status }}</p>
+      <p>{{ order.price }}</p>
+      <!-- <p>{{ order.userId }}</p>
+      <p>{{ order.storeId }}</p> -->
+      <p>{{ order.createdAt }}</p>
+      <p>{{ order.updatedAt }}</p>
+    </li>
+  </ul>
+
+  <div class="card">
+    
+  </div>
+</template>
+
+<style scoped>
+
+</style>
